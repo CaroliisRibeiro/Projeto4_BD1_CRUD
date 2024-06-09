abc5c3b675ea6605408bd79ad5cf1921383354ce
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2024-06-09 16:36:24 -0300

    Update views.py

--- api_rest/views.py
+++ api_rest/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Funcionario, Usuario, Professor, Disciplina, Aluno, Livro, Turma, Matricula
 from .serializers import FuncionarioSerializer, UsuarioSerializer, ProfessorSerializer, DisciplinaSerializer, AlunoSerializer, LivroSerializer, TurmaSerializer, MatriculaSerializer
-from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from django.shortcuts import render
-from sklearn.metrics.pairwise import linear_kernel
-from django.core.mail import send_mail
-from .models import Livro  # Importe o modelo Livro
-
+from .permissions import IsAdminOrReadOnly, IsFuncionarioOrReadOnly, IsProfessorOrReadOnly
 
 @api_view(['GET'])
+@permission_classes([IsAdminOrReadOnly])
 def get_funcionario_usuario_professor_aluno_disciplina_livros_turma(request):
     try:
         data = {}
@@ -28,6 +25,7 @@ def get_funcionario_usuario_professor_aluno_disciplina_livros_turma(request):
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAdminOrReadOnly])
 def funcionario_list_create(request):
     if request.method == 'GET':
         funcionarios = Funcionario.objects.all()
@@ -41,6 +39,7 @@ def funcionario_list_create(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
+@permission_classes([IsProfessorOrReadOnly])
 def listar_alunos_por_turma(request, turma_id):
     try:
         turma = get_object_or_404(Turma, pk=turma_id)
@@ -51,6 +50,7 @@ def listar_alunos_por_turma(request, turma_id):
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAdminOrReadOnly])
 def funcionario_detail_update_delete(request, id_funcionario):
     try:
         funcionario = get_object_or_404(Funcionario, pk=id_funcionario)
@@ -70,6 +70,7 @@ def funcionario_detail_update_delete(request, id_funcionario):
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAdminOrReadOnly])
 def usuario_list_create(request):
     if request.method == 'GET':
         usuarios = Usuario.objects.all()
@@ -83,6 +84,7 @@ def usuario_list_create(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAdminOrReadOnly])
 def usuario_detail_update_delete(request, id_usuario):
     try:
         usuario = get_object_or_404(Usuario, pk=id_usuario)
@@ -102,6 +104,7 @@ def usuario_detail_update_delete(request, id_usuario):
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAdminOrReadOnly])
 def professor_list_create(request):
     if request.method == 'GET':
         professores = Professor.objects.all()
@@ -115,6 +118,7 @@ def professor_list_create(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAdminOrReadOnly])
 def professor_detail_update_delete(request, id_prof):
     try:
         professor = get_object_or_404(Professor, pk=id_prof)
@@ -134,6 +138,7 @@ def professor_detail_update_delete(request, id_prof):
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAdminOrReadOnly])
 def disciplina_list_create(request):
     if request.method == 'GET':
         disciplinas = Disciplina.objects.all()
@@ -147,6 +152,7 @@ def disciplina_list_create(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAdminOrReadOnly])
 def disciplina_detail_update_delete(request, id_disciplina):
     try:
         disciplina = get_object_or_404(Disciplina, pk=id_disciplina)
@@ -166,6 +172,7 @@ def disciplina_detail_update_delete(request, id_disciplina):
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAdminOrReadOnly])
 def aluno_list_create(request):
     if request.method == 'GET':
         alunos = Aluno.objects.all()
@@ -179,6 +186,7 @@ def aluno_list_create(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAdminOrReadOnly])
 def aluno_detail_update_delete(request, id_aluno):
     try:
         aluno = get_object_or_404(Aluno, pk=id_aluno)
@@ -198,6 +206,7 @@ def aluno_detail_update_delete(request, id_aluno):
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAdminOrReadOnly])
 def livro_list_create(request):
     if request.method == 'GET':
         livros = Livro.objects.all()
@@ -211,6 +220,7 @@ def livro_list_create(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAdminOrReadOnly])
 def livro_detail_update_delete(request, id_livro):
     try:
         livro = get_object_or_404(Livro, pk=id_livro)
@@ -230,6 +240,7 @@ def livro_detail_update_delete(request, id_livro):
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAdminOrReadOnly])
 def turma_list_create(request):
     if request.method == 'GET':
         turmas = Turma.objects.all()
@@ -243,6 +254,7 @@ def turma_list_create(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAdminOrReadOnly])
 def turma_detail_update_delete(request, id_turma):
     try:
         turma = get_object_or_404(Turma, pk=id_turma)
@@ -262,6 +274,7 @@ def turma_detail_update_delete(request, id_turma):
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAdminOrReadOnly])
 def matricula_list_create(request):
     if request.method == 'GET':
         matriculas = Matricula.objects.all()
@@ -275,6 +288,7 @@ def matricula_list_create(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAdminOrReadOnly])
 def matricula_detail_update_delete(request, pk):
     try:
         matricula = get_object_or_404(Matricula, pk=pk)
@@ -294,6 +308,7 @@ def matricula_detail_update_delete(request, pk):
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET'])
+@permission_classes([IsProfessorOrReadOnly])
 def alunos_por_turma(request, id_turma):
     try:
         turma = get_object_or_404(Turma, pk=id_turma)
@@ -304,11 +319,14 @@ def alunos_por_turma(request, id_turma):
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET'])
+@permission_classes([IsProfessorOrReadOnly])
 def visualizar_alunos_matriculados(request):
     turmas = Turma.objects.all()
     data = {turma.nome_turma: [aluno.nome_aluno for aluno in turma.alunos_turma.all()] for turma in turmas}
     return Response(data)
 
+@api_view(['GET'])
+@permission_classes([IsProfessorOrReadOnly])
 def listar_alunos_por_turma(request, turma_id):
     try:
         turma = get_object_or_404(Turma, pk=turma_id)
@@ -317,27 +335,19 @@ def listar_alunos_por_turma(request, turma_id):
         return JsonResponse(alunos_list, safe=False)
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
-    
-    
 
 def obter_sugestoes_de_livros():
-    # Obtenha os últimos 5 livros adicionados como sugestões
     sugestoes = Livro.objects.order_by('-id')[:5]
 
-    # Agora você pode retornar essas sugestões
     return sugestoes
 
-
-
+@api_view(['GET'])
+@permission_classes([IsAdminOrReadOnly])
 def email_view(request):
-    # Lógica para obter sugestões de livros
     sugestoes = obter_sugestoes_de_livros()
 
-    # Lógica para determinar os destinatários do e-mail
-    # Aqui você pode obter os e-mails dos destinatários, por exemplo, de um modelo de usuário
     destinatarios = ['exemplo1@email.com', 'exemplo2@email.com']  # Substitua pelos e-mails reais dos destinatários
 
-    # Chame a função para enviar as sugestões por e-mail
     enviar_sugestoes_por_email(destinatarios, sugestoes)
 
     return render(request, 'template.html', {'sugestoes': sugestoes})
